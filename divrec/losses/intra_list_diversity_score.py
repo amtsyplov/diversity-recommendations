8910f58db9b10f9009c4dc4230ef5f10c7c689d4
alexey.tsyplov <alexey.tsyplov@corp.mail.ru>
2023-11-30 16:32:35 +0300

    Add IntraListBinaryUnfairnessScore

--- divrec/losses/intra_list_diversity_score.py
+++ divrec/losses/intra_list_diversity_score.py
@@ -15,15 +15,11 @@ class IntraListDiversityScore(ScoreWithReduction):
         ScoreWithReduction.__init__(self, *args, **kwargs)
         self.distance_matrix = distance_matrix
 
-    def forward(self, recommendations: torch.Tensor, distance_matrix: Optional[torch.Tensor] = None):
-        distances = self.distance_matrix if distance_matrix is None else distance_matrix
-        if distances is None:
-            raise ValueError("Distance matrix is not specified")
-
+    def forward(self, recommendations: torch.Tensor):
         no_user_recommendations = recommendations.size(1)
 
         loss_values = torch.Tensor(
-            [self.user_ild(user_recommendations, distances) for user_recommendations in recommendations]
+            [self.user_ild(user_recommendations, self.distance_matrix) for user_recommendations in recommendations]
         )
 
         loss_values /= no_user_recommendations * (no_user_recommendations - 1)
@@ -33,3 +29,14 @@ class IntraListDiversityScore(ScoreWithReduction):
     @staticmethod
     def user_ild(user_recommendations: torch.Tensor, distance_matrix: torch.Tensor) -> float:
         return sum(distance_matrix[i, j] for i, j in combinations(user_recommendations, 2))
+
+
+class IntraListBinaryUnfairnessScore(IntraListDiversityScore):
+    """
+
+    Controlling Popularity Bias in Learning to Rank Recommendation
+    by Himan Abdollahpouri, Robin Burke and others
+    """
+    def __init__(self, *args, item_labels: torch.LongTensor, **kwargs):
+        distance_matrix = (item_labels.unsqueeze(0) == item_labels.unsqueeze(1)).type(torch.IntTensor)
+        IntraListDiversityScore.__init__(self, *args, distance_matrix=distance_matrix, **kwargs)
--- divrec/metrics/__init__.py
+++ divrec/metrics/__init__.py
@@ -1,7 +1,7 @@
 from .auc_score import AUCScore
 from .average_precision_at_k import AveragePrecisionAtKScore, MeanAveragePrecisionAtKScore, average_precision_at_k
 from .entropy_diversity_score import EntropyDiversityScore
-from .intra_list_diversity_score import IntraListDiversityScore
+from divrec.losses.intra_list_diversity_score import IntraListDiversityScore
 from .normalized_discounted_cumulative_gain import NDCGScore, normalized_discounted_cumulative_gain
 from .precision_at_k import HitRateScore, PrecisionAtKScore, precision_at_k
 from .recall_at_k import RecallAtKScore, recall_at_k
