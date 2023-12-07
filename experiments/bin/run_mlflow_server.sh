817f32e785f38a880f07d096f0df98691a325472
alexey.tsyplov <alexey.tsyplov@corp.mail.ru>
2023-12-07 13:37:50 +0300

    Fix username

--- experiments/bin/run_mlflow_server.sh
+++ experiments/bin/run_mlflow_server.sh
@@ -1,4 +1,5 @@
 source ~/.virtualenvs/diversity-recommendations/bin/activate
 
-export MLFLOW_STORAGE=/Users/alexey.tsyplov/Projects/diversity-recommendations/experiments/.mlruns/
+export MLFLOW_STORAGE=/Users/${USER}/Projects/diversity-recommendations/experiments/.mlruns/
+echo $MLFLOW_STORAGE is used to storage experiments
 mlflow server --host 127.0.0.1 --port 8080 --backend-store-uri $MLFLOW_STORAGE --no-serve-artifacts
