743c23c506b8c0f5bd72bd663367598429d9bbd6
alexey.tsyplov <alexey.tsyplov@corp.mail.ru>
2023-12-09 14:29:19 +0300

    Finish with readme

--- experiments/scripts/create_readme.py
+++ experiments/scripts/create_readme.py
@@ -3,13 +3,13 @@ import click
 import pandas as pd
 
 metrics = [
-    "recall_atk_score_at_10",
-    "ndcg_score_at_10",
-    "precision_atk_score_at_10",
-    "pri_at_10",
-    "mean_average_precision_atk_score_at_10",
-    "entropy_diversity_score_at_10",
     "test_auc_score",
+    "recall_atk_score_at_10",
+    "precision_atk_score_at_10",
+    "mean_average_precision_atk_score_at_10",
+    "ndcg_score_at_10",
+    "pri_at_10",
+    "entropy_diversity_score_at_10",
 ]
 
 
@@ -20,12 +20,14 @@ def main(workdir: str, source: str) -> None:
     data = pd.read_csv(os.path.join(os.path.abspath(workdir), source))
     data = data.loc[data["metric"].isin(metrics)]
     data = data.pivot(columns="metric", index="experiment", values="value")
+    data = data[metrics]
 
     content = "## Current results:\n\n"
-    content += "| " + " | ".join(data.columns.tolist()) + " |\n"
-    content += "| " + " | ".join(["---" for _ in data.columns.tolist()]) + " |\n"
-    for row in data.values:
-        content += "| " + " | ".join([f"{v:.6f}" for v in row.tolist()]) + " |\n"
+    content += "| experiment | "
+    content += " | ".join([col.replace("score_", "").replace("atk_", "") for col in data.columns.tolist()]) + " |\n"
+    content += "| :--- | " + " | ".join(["---:" for _ in data.columns]) + " |\n"
+    for name, row in zip(data.index.tolist(), data.values):
+        content += f"| {name} | " + " | ".join([f"{v:.6f}" for v in row.tolist()]) + " |\n"
 
     with open(os.path.abspath("../README.md"), mode="w") as file:
         file.write(content)
